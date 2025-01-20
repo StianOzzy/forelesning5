f3b4c2f4f81beae2274d16d91c2437f606e2a623
stian <stian.olin@gmail.com>
2025-01-20 14:42:28 +0100

    Pressing Buttons

--- src/no/hiof/stianoo/forelesning5/Main.java
+++ src/no/hiof/stianoo/forelesning5/Main.java
@@ -1,7 +1,12 @@
 package no.hiof.stianoo.forelesning5;
 
+import no.hiof.stianoo.forelesning5.model.Course;
+import no.hiof.stianoo.forelesning5.model.Student;
+
 public class Main {
     public static void main(String[] args) {
 
+        Student student = new Student();
+        Course course = new Course();
     }
 }
